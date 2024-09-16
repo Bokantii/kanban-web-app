9f90ab2a4f476f5e85fe7998e65ae06f15ec98b9
USER <user@USERs-MacBook-Pro.local>
2024-09-16 00:29:03 -0400

    Updated Kanban Web App

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,15 +2,44 @@ import React from "react";
 import classes from "./Home.module.scss";
 import logoLight from "../../assets/logo-light.svg";
 import logoDark from "../../assets/logo-dark.svg";
+import IconCross from "../Icons/IconCross";
 const Home = () => {
   return (
     <section className={classes.home}>
-      <h1>Welcome to the Home Page</h1>
-      <section className={classes.side_bar}>
+      {/* header start */}
+      <section className={classes.header}>
         <div className={classes.logo}>
-          <img src={logoDark} alt="logo_light"/>
+          <img src={logoDark} alt="logo_light" />
         </div>
       </section>
+      {/* header end */}
+      {/* body start */}
+      <section className={classes.kanban_app_body}>
+        <section className={classes.side_bar}>
+          <section className={classes.side_bar_content}>
+            {/* All boards start */}
+            <section className={classes.all_boards}>
+              <span className={classes.all_boards_heading}>all boards</span>
+              <ul>
+                <li>
+                  <span>Platform Launch</span>
+                </li>
+                <li>
+                  <span>Marketing Plan</span>
+                </li>
+                <li>
+                  <span>Roadmap</span>
+                </li>
+                <li>
+                  <span>Create New Board</span>
+                </li>
+              </ul>
+            </section>
+            {/* All boards end */}
+          </section>
+        </section>
+      </section>
+      {/* body end */}
     </section>
   );
 };
